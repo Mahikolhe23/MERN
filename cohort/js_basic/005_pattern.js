dfc65f69173741c02ec16312ba80b29e61b37900
Mahikolhe23 <mkolhe23@gmail.com>
2023-11-29 16:49:02 +0000

    pattern

--- cohort/js_basic/005_pattern.js
+++ cohort/js_basic/005_pattern.js
@@ -5,11 +5,11 @@
 // ****
 // *****  
 for (let i = 0; i <= 5; i++) {
-    let str = "";
-    for (let j = 0; j <= i; j++) {
-        str += "*";
-    }
-    console.log(str);
+  let str = "";
+  for (let j = 0; j <= i; j++) {
+    str += "*";
+  }
+  console.log(str);
 }
 console.log("------------------------------------------------------------");
 // Question-2:  WAP to print this 
@@ -17,11 +17,21 @@ console.log("------------------------------------------------------------");
 //   ***
 //  *****
 // *******
-
-for (let i = 0; i < 5; i++) {
-    let str = "";
-    for (let j = 0; j < i / 2 - 1; j++) {
-        str += "*";
-    }
-    console.log(str);
+let rows = 5;
+for (let i = 1; i <= rows; i++) {
+  let str = "";
+  for (let j = 1; j <= rows - i; j++) {
+    str += " ";
+  }
+  for (let k = 0; k < 2 * i - 1; k++) {
+    str += "*";
+  }
+  console.log(str);
 }
+
+//Question-3: WAP to print this
+// *****
+// *   *
+// *   *
+// *****
+
