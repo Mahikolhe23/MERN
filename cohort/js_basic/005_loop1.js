45efd4be04a8bd5448e4b9b3d2c45719a6816f63
Mahikolhe23 <mkolhe23@gmail.com>
2023-11-29 15:56:52 +0000

    replit

--- cohort/js_basic/005_loop1.js
+++ cohort/js_basic/005_loop1.js
@@ -32,12 +32,12 @@ for (let i = 1; i < 7; i++) {
 
 
 //Question-6:  WAP to Develop a do-while loop for a simple number guessing game. Ask the user to guess a number between 1 and 10, and keep prompting until they guess correctly.
-let randomNumber = Math.floor(Math.random() * 10) + 1;
-let guess;
-do {
-  guess = prompt("Guess the number between 1 to 10");
-} while (guess != randomNumber);
-alert("You guess it");
+// let randomNumber = Math.floor(Math.random() * 10) + 1;
+// let guess;
+// do {
+//   guess = prompt("Guess the number between 1 to 10");
+// } while (guess != randomNumber);
+// alert("You guess it");
 
 //Question-7: WAP to display the multiplication table (1 to 10) in the console.
 for (let i = 1; i <= 10; i++) {
@@ -45,4 +45,31 @@ for (let i = 1; i <= 10; i++) {
 }
 
 //Question-8: WAP that use the break statement to exit the loop when the counter reaches 5. Also, use continue to skip printing the value 3 ( Note : Loop Starts from 0).
+let p = 1;
+while (p <= 10) {
+  if (p === 3) {
+    continue;
+    p++;
+  }
+  if (p === 5) {
+    break;
+  }
+  console.log(p);
+  p++;
+}
 
+//Question-9: WAP that logs numbers from 1 to 30. For multiples of 3, log "Fizz" instead of the number, and for multiples of 5, log "Buzz." For numbers which are multiples of both 3 and 5, log "FizzBuzz."
+for (let i = 1; i <= 30; i++) {
+  if (i % 3 == 0) {
+    console.log("Fizz");
+  }
+  if (i % 5 == 0) {
+    console.log("Buzz");
+  }
+  if (i % 3 == 0 || i % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+}
+
+//Question-10: WAP to identify and log prime numbers within a given range.
+function
