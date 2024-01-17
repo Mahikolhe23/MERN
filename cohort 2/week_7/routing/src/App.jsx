874d7d39ffca8f1994e7a33f033d847e8fc842e1
Mahendra Kolhe <mkolhe23@gmail.com>
2024-01-17 20:23:06 +0530

    react routing

---
+++ cohort 2/week_7/routing/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/Landing'
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <AppBar />
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/' element={<Landing />} />
+        </Routes>
+      </BrowserRouter> 
+    </div>
+  )
+}
+
+function AppBar() {
+  const navigate = useNavigate()
+  return <div>
+    <button onClick={() => {
+      navigate('/')
+    }}>Landing Page</button>
+    <button onClick={() => {
+      navigate('/dashboard')
+    }}>Dashboard</button>
+  </div>
+}
+
+export default App
