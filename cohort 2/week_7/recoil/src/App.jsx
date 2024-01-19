23ceee583aeab5c24ac830fe277af99b262ea0aa
Mahendra Kolhe <mkolhe23@gmail.com>
2024-01-19 22:10:40 +0530

    recoil

--- cohort 2/week_7/recoil/src/App.jsx
+++ cohort 2/week_7/recoil/src/App.jsx
@@ -1,13 +1,44 @@
 import { useState } from 'react'
 import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { countAtom, evenSelector } from './count'
 
 function App() {
-  const [count, setCount] = useState(0)
-  return (<div>
-    
-
-  </div>
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
   )
 }
 
+function Count() {
+  return <div>
+    <CountRenderer />
+    <Buttons />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom)
+  return <div>
+    {count}
+    <EvenCountRenderer />
+  </div>
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom)
+  return <div>
+    <button onClick={() => { setCount(count => count + 1) }}>Increase</button>
+    <button onClick={() => { setCount(count => count - 1) }}>Decrease</button>
+  </div>
+}
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector)
+  return <div>
+    {isEven ? "It is even" : null}
+  </div>
+}
 export default App
