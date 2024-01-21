891d029659dab35e9fada8a44c55d48fef34af38
Mahendra Kolhe <mkolhe23@gmail.com>
2024-01-21 11:04:37 +0530

    tailwind

---
+++ cohort 2/week_8/tailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='grid grid-cols-1 grid md:grid-cols-3'>
+      <div className='bg-red-500'>Hi There 1</div>
+      <div className='bg-green-500'>Hi There 2</div>
+      <div className='bg-blue-500'>Hi There 3</div>
+    </div>
+  )
+}
+
+export default App
